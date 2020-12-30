bf9b30988986820f77bf3f20f0aaa99e286eb202
Juan Ignacio <juanrios0495@gmail.com>
2020-12-29 23:08:13 -0300

    Connect MongoDB with Node. Clouster running :)

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // Manejo de ambientes de produccion y desarrollo
 const dotenv = require('dotenv');
 const express = require('express');
+const { Server } = require('http');
 const morgan = require('morgan');
-
-const libro = require('./rutas/libros');
+const connectDataBase = require('./config/db');
 
 // Ubicacion de las variables globales
 dotenv.config({path: './config/config.env'});
+connectDataBase();
+
+const libro = require('./rutas/libros');
+
 
 const app = express();
 
@@ -27,7 +31,12 @@ app.use('/api/Libro', libro);
 // PORT esta defenido en el archivo config.env
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log('App listening on port', PORT, 
     'and running in', process.env.NODE_ENV, 'environment!');
+});
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log("Errores:", err.message);
+    server.close(() => process.exit(1));
 });
